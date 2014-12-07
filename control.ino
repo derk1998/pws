9d655b82eef86cf02db362557717da1bf57e9fe0
derkos <derkwiegerinck@gmail.com>
2014-12-07 20:02:27 +0100

    Update control.ino

--- control.ino
+++ control.ino
@@ -1,13 +1,12 @@
-//Deze code is gemaakt door: Derk Wiegerinck
-
-#include <Servo.h>
+#include <Servo.h> 
 
 Servo m1;
 Servo m2;
 
-int incomingByte; 
-const int r1 = 2;
-const int r2 = 13;
+int incomingByte;
+
+const int r1 = 3;
+const int r2 = 2;
 
 int reverse1 = 0;
 int reverse2 = 0;
@@ -15,178 +14,151 @@ int reverse2 = 0;
 void setup()
 {
   Serial.begin(9600);
-  m1.attach(9); //motor 1 op pin 9
-  m2.attach(10); //motor 2 op pin 10 
   pinMode(r1, OUTPUT);
   pinMode(r2, OUTPUT);
-  m1.write(0);
-  m2.write(0);
+  m1.attach(9);
+  m2.attach(10);
+  m1.write(36);
+  m2.write(36);
+ 
 }
-  
-  void loop()
-  {
-  //zoeken naar seriële data
+
+void loop()
+{
   if (Serial.available() > 0)
   {
     incomingByte = Serial.read();
-    
-    //joystick 1
     while(incomingByte == 'a')
-    {   
-        incomingByte = Serial.read(); 
-        m1.write(0);
-        break;  
+    { 
+      incomingByte = Serial.read();  
+      m2.write(36);
+      break;
     }
     while(incomingByte == 'b')
-    {      
+    {   
         incomingByte = Serial.read(); 
-        m1.write(20);
+        m2.write(46);
         break;  
     }
     while(incomingByte == 'c')
     {   
         incomingByte = Serial.read(); 
-        m1.write(40);
+        m2.write(56);
         break;  
     }
     while(incomingByte == 'd')
     {   
         incomingByte = Serial.read(); 
-        m1.write(60);
+        m2.write(66);
         break;  
     }
     while(incomingByte == 'e')
     {   
         incomingByte = Serial.read(); 
-        m1.write(80);
+        m2.write(76);
         break;  
     }
     while(incomingByte == 'f')
     {   
         incomingByte = Serial.read(); 
-        m1.write(100);
-        break;  
-    } 
-     while(incomingByte == 'g')
-    {   
-        incomingByte = Serial.read(); 
-        m1.write(120);
+        m2.write(86);
         break;  
     }
-     while(incomingByte == 'h')
+    while(incomingByte == 'g')
     {   
         incomingByte = Serial.read(); 
-        m1.write(140);
+        m2.write(96);
         break;  
     }
-     while(incomingByte == 'i')
+    while(incomingByte == 'h')
     {   
         incomingByte = Serial.read(); 
-        m1.write(160);
+        m2.write(106);
         break;  
     }
-     while(incomingByte == 'j')
+    
+    
+    
+    while(incomingByte == 'A')
+    { 
+      incomingByte = Serial.read();  
+      m1.write(36);
+      break;
+    }
+    while(incomingByte == 'B')
     {   
         incomingByte = Serial.read(); 
-        m1.write(180);
+        m1.write(46);
         break;  
     }
-     //vooruit joystick 2
-     while(incomingByte == 'k')
+    while(incomingByte == 'C')
     {   
         incomingByte = Serial.read(); 
-        m2.write(0);
+        m1.write(56);
         break;  
     }
-    while(incomingByte == 'l')
+    while(incomingByte == 'D')
     {   
         incomingByte = Serial.read(); 
-       m2.write(20);
+        m1.write(66);
         break;  
     }
-    while(incomingByte == 'm')
+    while(incomingByte == 'E')
     {   
         incomingByte = Serial.read(); 
-        m2.write(40);
+        m1.write(76);
         break;  
     }
-    while(incomingByte == 'n')
+    while(incomingByte == 'F')
     {   
         incomingByte = Serial.read(); 
-        m2.write(60);
+        m1.write(86);
         break;  
     }
-    while(incomingByte == 'o')
+    while(incomingByte == 'G')
     {   
         incomingByte = Serial.read(); 
-        m2.write(80);
+        m1.write(96);
         break;  
     }
-    while(incomingByte == 'p')
+    while(incomingByte == 'H')
     {   
         incomingByte = Serial.read(); 
-        m2.write(100);
-        break;  
-    } 
-    while(incomingByte == 'q')
-    {   
-        incomingByte = Serial.read(); 
-        m2.write(120);
+        m1.write(106);
         break;  
     }
-    while(incomingByte == 'r')
-    {   
-        incomingByte = Serial.read(); 
-        m2.write(140);
-        break;  
-    }
-    while(incomingByte == 's')
-    {   
-        incomingByte = Serial.read(); 
-        m2.write(160);
-        break;  
-    }
-    while(incomingByte == 't')
-    {   
-        incomingByte = Serial.read(); 
-        m2.write(180);
-        break;  
-    } 
     
     //relay 1 wordt ingeschakeld
-    while(incomingByte == '9' && reverse1 == 0)
+    while(incomingByte == '3' && reverse1 == 0)
     {
        incomingByte = Serial.read();
        digitalWrite(r1, HIGH);
        reverse1 = 1;
        break;
     }
-    
-    //relay 2 wordt ingeschakeld
-    while(incomingByte == '8' && reverse2 == 0)
-    {
-        incomingByte = Serial.read();
-        digitalWrite(r2, HIGH);
-        reverse2 = 1;
-        break;
-    }
-  
     //relay 1 wordt uitgeschakeld
-    while(incomingByte == '6' && reverse1 == 1)
+    while(incomingByte == '4' && reverse1 == 1)
     {
         incomingByte = Serial.read();
         digitalWrite(r1, LOW);
         reverse1 = 0;
         break;
     }
- 
+    //relay 2 wordt ingeschakeld
+    while(incomingByte == '7' && reverse2 == 0)
+    {
+        incomingByte = Serial.read();
+        digitalWrite(r2, HIGH);
+        reverse2 = 1;
+        break;
+    }
     //relay 2 wordt uitgeschakeld
-    while(incomingByte == '7' && reverse2 == 1)
+    while(incomingByte == '8' && reverse2 == 1)
     {
         incomingByte = Serial.read();
         digitalWrite(r2, LOW);
         reverse2 = 0;
         break;
-    }   
+    }
   }
 }
